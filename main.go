de2cb0d292cde38b2c277e89ea8187c977fe2a1b
Phantomvv1 <krisi_andreev@abv.bg>
2025-10-26 16:16:48 +0200

    Finished the program

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
-	_ "image/jpeg"
-	_ "image/png"
+	"image/jpeg"
+	"image/png"
 	"log"
 	"os"
 )
@@ -40,11 +39,35 @@ func equalizeHistogram(img image.Image, format string) {
 	numberOfIntenistyLevels := 256
 	totalPixels := img.Bounds().Dx() * img.Bounds().Dy()
 	hMid := totalPixels / numberOfIntenistyLevels // Hmid
+
 	coloursHistogram := countColours(img)
-	newIndex := 0     // Rver
-	histogramSum := 0 // Hsum
-	fmt.Println(hMid)
-	fmt.Println(format)
+
+	CnR := equalizeColour(coloursHistogram.R, hMid)
+	CnG := equalizeColour(coloursHistogram.G, hMid)
+	CnB := equalizeColour(coloursHistogram.B, hMid)
+
+	newImg := makeNewImage(CnR, CnG, CnB, img)
+
+	file, err := os.Create("out." + format)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	switch format {
+	case "png":
+		err = png.Encode(file, newImg)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	case "jpg":
+		err = jpeg.Encode(file, newImg, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
 func countColours(img image.Image) RGBHistogram {
@@ -61,4 +84,43 @@ func countColours(img image.Image) RGBHistogram {
 	}
 
 	return result
+}
+
+func equalizeColour(histogram [256]int, hMid int) [256]int {
+	var Cn [256]int
+	newIndex := 0     // Rver
+	histogramSum := 0 // Hsum
+
+	for i := range 256 {
+		l := newIndex
+		histogramSum += histogram[i]
+
+		if histogramSum > hMid {
+			histogramSum -= hMid
+			newIndex++
+		}
+
+		r := newIndex
+		Cn[i] = (l + r) / 2
+	}
+
+	return Cn
+}
+
+func makeNewImage(CnR, CnG, CnB [256]int, img image.Image) image.Image {
+	newImg := image.NewRGBA(img.Bounds())
+
+	// Mapping the old colours to the new colours and setting them in the new image
+	for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+		for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			newR := uint8(CnR[r>>8])
+			newG := uint8(CnG[g>>8])
+			newB := uint8(CnB[b>>8])
+
+			newImg.Set(x, y, color.RGBA{R: newR, G: newG, B: newB, A: uint8(a >> 8)})
+		}
+	}
+
+	return newImg
 }
